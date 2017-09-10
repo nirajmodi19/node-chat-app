0762d885e26c80d0cdd226735c0ea7ed9f1289fe
Niraj Modi <nirajmodi51@gmail.com>
2017-09-10 11:24:49 +0530

    Add newMessage and createMessage

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+var socket = io();
+
+//".on" is used for event listening & ".emit" is used to emit event
+
+socket.on('connect', function () {		
+	console.log('Connected to server');	
+
+	/*socket.emit('createEmail', {
+		to: 'dhiraj@mail.com',
+		text: 'heya'
+	});*/
+
+	socket.emit('createMessage', {
+		from: 'xyz@mail.com',
+		text: 'heya'
+	});
+
+});
+
+socket.on('newMessage', function (message) {
+	console.log('New message recieved: ', message);
+});
+
+socket.on('disconnect', function() {
+	console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email) {
+	console.log('New Email', email);
+});
