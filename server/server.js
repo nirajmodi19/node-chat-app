c239760fd206c6b6e15b6b41197f9a8bd810be54
Niraj Modi <nirajmodi51@gmail.com>
2017-09-12 20:00:41 +0530

    Add a join page

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
 	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
 	socket.on('createMessage', (message, callback) => {
-		console.log('createMessage: ',message);
+		//console.log('createMessage: ',message);
 		io.emit('newMessage', generateMessage(message.from, message.text)); 
 		callback();
 	});
