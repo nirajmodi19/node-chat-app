f2564cba8d4367213a914810a5e58fe6de078010
Niraj Modi <nirajmodi51@gmail.com>
2017-09-12 18:46:57 +0530

    Add Autoscrolling to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+	// Selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	// Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+	
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 //".on" is used for event listening & ".emit" is used to emit event
 
 socket.on('connect', function () {		
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -35,14 +52,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
-	// var li = jQuery('<li></li>');
-	// var a =jQuery('<a target="_blank">My current location</a>');
-
-	// li.text(`${message.from} ${formattedTime}: `);
-	// a.attr('href', message.url);
-
-	// li.append(a);
-	// jQuery('#messages').append(li);
+	scrollToBottom();
 });
 
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
